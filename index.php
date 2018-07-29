9c8ef9973a73ac0253bf2ff4129b938a91dcae4f
Fernando Monroy <pulmundo@gmail.com>
2018-07-29 17:56:47 -0600

    Archivos y carpeta

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="https://www.efpemusac.org/">EFPEM<img src="img/logo.png" alt="LogoTipo"></a> 
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Inicio</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="acerca_de.html">Acerca de</a></li>
+					<li><a href="contacto.html">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos!</h1>
+			</div>
+ 
+			<div class="articulo">
+				<article>
+					<p>Visión: <br/>Ser la unidad académica de la Universidad de San Carlos de Guatemala que&nbsp; coadyuva a la formación del profesional docente del sistema educativo nacional, con bases científica, tecnológica y humanística, que promuevan una cultura ambiental sostenible; que se fundamenten en las nuevas corrientes pedagógicas, capaces de cubrir las necesidades de la educación nacional en un contexto intercultural, cimentados en los valores éticos y morales de un profesional de la educación. Contamos con un cuerpo docente altamente calificado y personal administrativo capacitado al servicio de nuestros usuarios.</p>
+					<br/>
+					<p>Misión: <br/>Somos una Escuela no facultativa de la Universidad de San Carlos de Guatemala, con excelencia académica en el funcionamiento de todos sus programas, responsable de la formación de profesionales de alta calidad, en el campo de la docencia, en los distintos niveles del sistema educativo nacional, y que, basados en la investigación, la docencia y la extensión, contribuimos al fortalecimiento del proceso educativo en el ámbito nacional, trabajando con ética, responsabilidad y compromiso con el desarrollo ambiental sostenible</p>
+					<br/>
+					<p>Ser la unidad académica de la Universidad de San Carlos de Guatemala que&nbsp; coadyuva a la formación del profesional docente del sistema educativo nacional, con bases científica, tecnológica y humanística, que promuevan una cultura ambiental sostenible; que se fundamenten en las nuevas corrientes pedagógicas, capaces de cubrir las necesidades de la educación nacional en un contexto intercultural, cimentados en los valores éticos y morales de un profesional de la educación. Contamos con un cuerpo docente altamente calificado y personal administrativo capacitado al servicio de nuestros usuarios.</p>
+				</article>
+			</div>
+ 
+			<aside>
+				<div class="widget">
+					<h3>Carreras</h3>
+					<ul>
+						<li><a href="#">Profesorado en Informatica</a></li>
+						<li><a href="#">Profesorado en Matematica y Fisica</a></li>
+						<li><a href="#">Profesorado en Quimica</a></li>
+						<li><a href="#">Profesorado en Economico Contable</a></li>
+						<li><a href="#">Profesorado en Lengua y Literatura</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+			<p>https://www.efpemusac.org/</p>
+		</div>
+	</footer>
+</body>
+</html>
