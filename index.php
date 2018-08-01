b4bdcf40ba4bcf38ac734dbcdef28d4e26e8d81e
Fernando Reyes Monroy Martnez <pulmundo@gmail.com>
2018-08-01 09:17:08 -0600

    Agregado de descripcion de producto

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 							<div class="imagen-producto"><img src="img/bocinas.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Speakers s150 digital usb</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Bocinas de 5.1 Canales. Con filtro de sonido. Conexión USB
 							</div>
 						</div>
 					</div>
@@ -39,7 +39,7 @@
 							<div class="imagen-producto"><img src="img/cable_red.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Ethernet cable</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Cable de Red de 5mts de longitud. Cat5e. Con recubrimiento.
 							</div>
 						</div>
 					</div>
@@ -48,7 +48,7 @@
 							<div class="imagen-producto"><img src="img/disco_duro.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Hard disk</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Disco Duro Meptor. 500Gb SATA.
 							</div>
 						</div>
 					</div>
@@ -57,7 +57,7 @@
 							<div class="imagen-producto"><img src="img/impresora.gif" alt=""></div>
 							<div class="nombre-producto"><a href="#">Epson stylus</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Impresora EPSON. Inyeccion de Tinta. Escaner.
 							</div>
 						</div>
 					</div>
@@ -66,7 +66,7 @@
 							<div class="imagen-producto"><img src="img/jostick.gif" alt=""></div>
 							<div class="nombre-producto"><a href="#">Logitech extreme 3d pro - mando joystick</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Mando para juegos arcade de Vuelo. Base plastica con luces led y vibracion de movimiento.
 							</div>
 						</div>
 					</div>
@@ -75,7 +75,7 @@
 							<div class="imagen-producto"><img src="img/lcd_monitor.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Led-backlit lcd display</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Monitor LCD de 19'. Sistema de Ahorro. Base plástica. 
 							</div>
 						</div>
 					</div>
@@ -84,7 +84,7 @@
 							<div class="imagen-producto"><img src="img/mouse.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Mouse</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Mouse innalambrico. Color Gris. Mediano.
 							</div>
 						</div>
 					</div>
@@ -93,16 +93,16 @@
 							<div class="imagen-producto"><img src="img/protector.gif" alt=""></div>
 							<div class="nombre-producto"><a href="#">Filtros para el monitor</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Filtro de pantalla de 19' para monitores Cuadrados o semicuadrados.
 							</div>
 						</div>
 					</div>
 					<div class="productos">
 						<div class="principal">
 							<div class="imagen-producto"><img src="img/switch.png" alt=""></div>
-							<div class="nombre-producto"><a href="#">Computer hardware</a></div>
+							<div class="nombre-producto"><a href="#">Switch Ethernet</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Switch de 8 puertos con anidacion máximo de 4.
 							</div>
 						</div>
 					</div>
@@ -111,7 +111,7 @@
 							<div class="imagen-producto"><img src="img/tarjeta_madre.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Tarjeta madre</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Tarjeta para gamers. Sistema de Sockets de Memoria RAM max 16 GB. Sockets de Tarjeta de Video Max 4GB.
 							</div>
 						</div>
 					</div>
@@ -120,7 +120,7 @@
 							<div class="imagen-producto"><img src="img/tarjeta_red.gif" alt=""></div>
 							<div class="nombre-producto"><a href="#">Tarjeta de red</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Tarjeta ethernet con 10/100. Ranura PCI Express.
 							</div>
 						</div>
 					</div>
@@ -129,7 +129,7 @@
 							<div class="imagen-producto"><img src="img/teclado.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">logitech wireless touch keyboard k400</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Teclado innalambrico 3 metros de alcance. Sistema Touch para las funciones del mouse.
 							</div>
 						</div>
 					</div>
@@ -138,7 +138,7 @@
 							<div class="imagen-producto"><img src="img/ups.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">UPS</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								UPS Forza de 500VA. Con 4 Conexiones. Sistema de Carga Lite.
 							</div>
 						</div>
 					</div>
@@ -147,7 +147,7 @@
 							<div class="imagen-producto"><img src="img/webcam.png" alt=""></div>
 							<div class="nombre-producto"><a href="#">Webcam</a></div>
 							<div class="descripcion-producto">
-								Windows 10. 1TB Disco Duro. 8 GB RAM. Wireless. Pantalla Touch 14'
+								Color Blanco con sistema de grabación de 720p. Conexcion USB.
 							</div>
 						</div>
 					</div>
